4772c725b181c2359e1ad1a7e8b59d1d0fb97dff
novus318 <nizamudheen318@gmail.com>
2025-08-28 22:02:38 +0530

    urls changes

--- utilities/AxiosInterceptor.ts
+++ utilities/AxiosInterceptor.ts
@@ -2,8 +2,8 @@ import { API_ENDPOINTS } from "@/lib/apiEndpoints";
 import axios, { AxiosRequestConfig } from "axios";
 import Cookies from "js-cookie";
 
-// const API_URL = "https://server.momenz.in";
-const API_URL = "http://localhost:5000";
+const API_URL = "https://server.momenz.in";
+// const API_URL = "http://localhost:5000";
 
 const axiosApi = axios.create({
   baseURL: API_URL,
